4ec62de0625db149432b45c5b99589e21e5b59f7
yj1722 <yj1722@imperial.ac.uk>
2023-10-15 21:57:58 +0100

    implementation of fix-point.h handling the Fixed-Point Real Arithmetic

---
+++ src/threads/fixed-point.h
@@ -0,0 +1,48 @@
+/* fixed-point.h -- header file for floating point implementation */
+#ifndef PINTOS_7_FIXED_POINT_H
+#define PINTOS_7_FIXED_POINT_H
+
+#include "lib/stdint.h"
+#include "lib/inttypes.h"
+#include "lib/assert.h"
+
+#define FLOATING_Q 14                     /* decimal part for real numbers */
+#define FLOATING_F (1 << FLOATING_Q)      /* integer part for real numbers */
+
+/* using int represent floating points */
+typedef int32_t fp;
+
+/* basic function for floating points */
+static fp fp_construct (int numerator, int denominator) {
+  return (numerator * FLOATING_F) / denominator;
+}
+
+static int fp_rounding_down (fp real_number) {
+  return real_number / FLOATING_F;
+}
+
+static int fp_rounding_near (fp real_number) {
+  return (real_number >= 0) ? (real_number + FLOATING_F / 2) / FLOATING_F :
+         (real_number - FLOATING_F / 2) / FLOATING_F;
+}
+
+/* operator for floating point numbers addition and subtraction */
+static fp fp_add (fp real_number, int int_number) {
+  return real_number + int_number * FLOATING_F;
+}
+
+static fp fp_subtract (fp real_number, int int_number) {
+  return real_number - int_number * FLOATING_F;
+}
+
+/* operator for floating point multiplication and division      */
+static fp fp_multiply (fp real1, fp real2) {
+  return ((int64_t) real1) * real2 / FLOATING_F;
+}
+
+static fp fp_divide (fp real1, fp real2) {
+  ASSERT (real2 != 0);
+  return ((int64_t) real1) * FLOATING_F / real2;
+}
+
+#endif //PINTOS_7_FIXED_POINT_H
