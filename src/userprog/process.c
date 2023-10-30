a2d60cc8c520724577b6bad417364d6722396e3d
Yi1lan <2468703258@qq.com>
2023-10-30 15:50:59 +0000

    [yw6822] "Argument Passing for process_execute()"
    Adding function <strtok_r> to <process_execute> making it support passing arguments.

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -38,6 +38,10 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
 
+  /* Argument Passing. */
+  char *rest;
+  file_name = strtok_r ((char *) file_name, " ", &rest);
+
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
